0b37d4390a331cb297eb21cf2d8da2777ac657d6
checkur5 <teamcheckur5@gmail.com>
2023-04-05 17:26:59 -0500

    Add AI main

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-
 from checkers.board import Board
 from checkers.game import Game
+from minimax.algorithm import minimax
 
-# checkerboard = Board()
-
-# piece = checkerboard.get_piece(0,1)
-
-# # checkerboard.move_piece(piece, 4, 3)
-
-# checkerboard.print_board()
 
 game = Game()
 
-#game.select(2, 1)
-#game.select(7, 9)
+new_board = Board()
 
-game.robot.grab_piece((6, 7))
-game.robot.drop_in_box()
+new_board.print_board()
+print("\n")
+
+# checkerboard.move_piece(piece, 4, 3)
+
+# checkerboard.print_board()
+
+value, new_board = minimax(game.get_board(), 2, "orange", game)
+game.ai_move(new_board)
+
+new_board.print_board()
+print("\n")
+
+
+#lol i thought that this would move the human piece but it didnt!
+#the computer do be making it's own moves tho
+# piece = checkerboard.get_piece(5,4)
+# new_board.move_piece(piece, 4, 5)
+
+# value, new_board = minimax(game.get_board(), 2, "orange", game)
+# game.ai_move(new_board)
+
+# new_board.print_board()
+
+game.select(5,4)
+game.select(4,5)
+new_board.print_board()
+print("\n")
